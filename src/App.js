4b1d8ae3d3ceeb94c9b24b028b25e7489954fbbb
Alex Volo <alex_volo@bk.ru>
2023-05-30 15:04:00 +0900

    Start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./Main.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <wrapper className="wrapper">
+            <header>
+                <img className="header__logo" src={logo} alt="logo"></img>
+            </header>
+            <nav>
+                <ul>
+                    <li>Profile</li>
+                    <li>Messages</li>
+                </ul>
+            </nav>
+            <main></main>
+        </wrapper>
+    );
 }
 
 export default App;
