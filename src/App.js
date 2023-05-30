2d8d4f76fb389988575f893396588c6a8db94244
Alex Volo <alex_volo@bk.ru>
2023-05-30 16:21:58 +0900

    pause

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import logo from "./logo.svg";
 import "./Main.css";
+import mainImage from './img/main-top.jpg'
 
 function App() {
     return (
-        <wrapper className="wrapper">
-            <header>
+        <wrapper className="app-wrapper">
+            <header className="header">
                 <img className="header__logo" src={logo} alt="logo"></img>
             </header>
-            <nav>
-                <ul>
-                    <li>Profile</li>
-                    <li>Messages</li>
+            <nav className="navigation">
+                <ul className="navigation__ul">
+                    <li className="navigation__li">
+                        <a href="# ">Profile</a>
+                    </li>
+                    <li className="navigation__li">
+                        <a href="# ">Messages</a>
+                    </li>
+                    <li className="navigation__li">
+                        <a href="# ">News</a>
+                    </li>
+                    <li className="navigation__li">
+                        <a href="# ">Music</a>
+                    </li>
+                    <li className="navigation__li">
+                        <a href="# ">Settings</a>
+                    </li>
                 </ul>
             </nav>
-            <main></main>
+            <main className="main">
+              <img className="main__img" src={mainImage} alt="main" />
+            </main>
         </wrapper>
     );
 }
