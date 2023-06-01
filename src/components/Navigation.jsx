ab43292561285dcf90aeb9dd5e12c32ee3c5ffd9
Alex Volo <alex_volo@bk.ru>
2023-06-01 20:51:05 +0900

    Navigation: added module-css

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,21 +1,34 @@
+import style from "./Navigation.module.css";
+// import "./Navigation.css";
+
 const Navigation = () => {
   return (
-    <nav className="navigation">
-      <ul className="navigation__ul">
-        <li className="navigation__li">
-          <a className="navigation__item" href="# ">Profile</a>
+    <nav className={style.nav}>
+      <ul>
+        <li>
+          <a className={style.item} href="# ">
+            Profile
+          </a>
         </li>
-        <li className="navigation__li">
-          <a className="navigation__item" href="# ">Messages</a>
+        <li>
+          <a className={style.item} href="# ">
+            Messages
+          </a>
         </li>
-        <li className="navigation__li">
-          <a className="navigation__item" href="# ">News</a>
+        <li>
+          <a className={style.item} href="# ">
+            News
+          </a>
         </li>
-        <li className="navigation__li">
-          <a className="navigation__item" href="# ">Music</a>
+        <li>
+          <a className={style.item} href="# ">
+            Music
+          </a>
         </li>
-        <li className="navigation__li">
-          <a className="navigation__item" href="# ">Settings</a>
+        <li>
+          <a className={style.item} href="# ">
+            Settings
+          </a>
         </li>
       </ul>
     </nav>
