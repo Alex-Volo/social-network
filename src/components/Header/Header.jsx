4de41623ffb9bc1fe4b9aec5509c35d93c4e84a7
Alex Volo <alex_volo@bk.ru>
2023-06-02 15:52:02 +0900

    Post: added module, module-css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import style from "./Header.module.css";
 const Header = () => {
   return (
     <header className={style.header}>
-      <img className={style.logo} src={logo} alt="logo"></img>
+      <img className={style.logo} src={logo} alt="logo" />
     </header>
   );
 };
--- src/components/Profile/Posts/Post/Post.jsx
+++ src/components/Profile/Posts/Post/Post.jsx
@@ -1,7 +1,14 @@
 import style from "./Post.module.css";
+import avatar from "../../avatar.png";
+// src\components\Profile\avatar.png
 
 const Profile = () => {
-    return <div className={style.post}>Пост 1</div>;
+    return (
+        <div className={style.post}>
+            <img className={style.avatar} src={avatar} alt="Ava" />
+            <div className={style.text}>Пост 1</div>
+        </div>
+    );
 };
 
 export default Profile;
