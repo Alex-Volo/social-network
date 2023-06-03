9ba33bef280f17b8bbe585d58032dc949df56c67
Alex Volo <alex_volo@bk.ru>
2023-06-04 08:07:42 +0900

    Post: added props(text, likesCount)

--- src/components/Profile/Posts/Post/Post.jsx
+++ src/components/Profile/Posts/Post/Post.jsx
@@ -2,13 +2,16 @@ import style from "./Post.module.css";
 import avatar from "../../avatar.png";
 // src\components\Profile\avatar.png
 
-const Profile = () => {
-    return (
-        <div className={style.post}>
-            <img className={style.avatar} src={avatar} alt="Ava" />
-            <div className={style.text}>Пост 1</div>
-        </div>
-    );
+const Profile = (props) => {
+  return (
+    <div className={style.post}>
+      <img className={style.avatar} src={avatar} alt="Ava" />
+      <div className={style.text}>
+        <p>{props.text}</p>
+        <p>Likes: {props.likesCount}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
--- src/components/Profile/Posts/Posts.jsx
+++ src/components/Profile/Posts/Posts.jsx
@@ -2,19 +2,19 @@ import style from "./Posts.module.css";
 import Post from "./Post/Post.jsx";
 
 const Profile = () => {
-    return (
-        <div className={style.posts}>
-            <p>My posts</p>
-            <div className={style.addPost}>
-                <textarea></textarea>
-                <button>Add post</button>
-            </div>
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-        </div>
-    );
+  return (
+    <div className={style.posts}>
+      <p>My posts</p>
+      <div className={style.addPost}>
+        <textarea></textarea>
+        <button>Add post</button>
+      </div>
+      <Post text="Пусть здесь будет текст" likesCount="0" />
+      <Post text="Разные посты" likesCount="20" />
+      <Post text="Будут лежать" likesCount="5" />
+      <Post text="Здесь" likesCount="7" />
+    </div>
+  );
 };
 
 export default Profile;
