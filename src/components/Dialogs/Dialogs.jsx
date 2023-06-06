0a950b2a84ca388896a0d616cea378504d6a4cac
Alex Volo <alex_volo@bk.ru>
2023-06-06 11:23:38 +0900

    Dialogs: component refactored

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,49 +1,37 @@
 import { NavLink } from "react-router-dom";
 import style from "./Dialogs.module.css";
 
-const Dialogs = () => {
-  return (
-    <div className={style.dialogs_wrapper}>
-      <header className={style.header}>
-        <h1>Dialogs</h1>
-      </header>
-      <div className={style.dialogs}>
-        <div className="dialog">
-          <NavLink className={style.dialog__name} to="/dialogs/1">
-            Artur
-          </NavLink>
-        </div>
-        <div className="dialog">
-          <NavLink className={style.dialog__name} to="/dialogs/2">
-            Alex
-          </NavLink>
-        </div>
-        <div className="dialog">
-          <NavLink className={style.dialog__name} to="/dialogs/3">
-            Dmitriy
-          </NavLink>
-        </div>
-        <div className="dialog">
-          <NavLink className={style.dialog__name} to="/dialogs/4">
-            Fedor
-          </NavLink>
-        </div>
-        <div className="dialog">
-          <NavLink className={style.dialog__name} to="/dialogs/5">
-            Evgeniy
-          </NavLink>
-        </div>
-      </div>
-
-      <div className="messages">
-        <p>Message 1</p>
-        <p>Message 2</p>
-        <p>Message 3</p>
-        <p>Message 4</p>
-        <p>Message 5</p>
-      </div>
+const DialogItem = (props) => (
+    <div className="dialog">
+        <NavLink className={style.dialog__item} to={"/dialogs/" + props.id}>
+            {props.name}
+        </NavLink>
     </div>
-  );
+);
+
+const Dialogs = () => {
+    return (
+        <div className={style.dialogs_wrapper}>
+            <header className={style.header}>
+                <h1>Dialogs</h1>
+            </header>
+            <div className={style.dialogs}>
+                <DialogItem name="Artur" id="1" />
+                <DialogItem name="Alex" id="2" />
+                <DialogItem name="Dmitriy" id="3" />
+                <DialogItem name="Fedor" id="4" />
+                <DialogItem name="Evgeniy" id="5" />
+            </div>
+
+            <div className="messages">
+                <p>Message 1</p>
+                <p>Message 2</p>
+                <p>Message 3</p>
+                <p>Message 4</p>
+                <p>Message 5</p>
+            </div>
+        </div>
+    );
 };
 
 export default Dialogs;
