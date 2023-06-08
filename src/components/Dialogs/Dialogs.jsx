6be001d8a086ce2afc42d832559dc533c1f5c836
Alex Volo <alex_volo@bk.ru>
2023-06-08 09:16:30 +0900

    working with DialogItem

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,24 +3,23 @@ import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
 
 const Dialogs = (props) => {
-  debugger;
-  return (
-    <div className={style.dialogs_wrapper}>
-      <header className={style.header}>
-        <h1>Dialogs</h1>
-      </header>
-      <div className={style.dialogs}>
-        {props.dialogsState.dialogsData.map((dia) => (
-          <DialogItem name={dia.name} id={dia.id} />
-        ))}
-      </div>
-      <div ClassName="messages">
-        {props.dialogsState.messagesData.map((mes) => (
-          <Message message={mes.message} />
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div className={style.dialogs_wrapper}>
+            <header className={style.header}>
+                <h1>Dialogs</h1>
+            </header>
+            <div className={style.dialogs}>
+                {props.dialogsState.dialogsData.map((dia) => (
+                    <DialogItem name={dia.name} id={dia.id} />
+                ))}
+            </div>
+            <div className="messages">
+                {props.dialogsState.messagesData.map((mes) => (
+                    <Message message={mes.message} />
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default Dialogs;
