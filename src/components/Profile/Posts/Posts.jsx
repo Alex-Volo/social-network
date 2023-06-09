1f1def1cd54362110d371bd9811fef80bdd85bf0
Alex Volo <alex_volo@bk.ru>
2023-06-09 23:45:48 +0900

    Posts: added onclick

--- src/components/Profile/Posts/Posts.jsx
+++ src/components/Profile/Posts/Posts.jsx
@@ -1,13 +1,20 @@
 import style from "./Posts.module.css";
 import Post from "./Post/Post.jsx";
+import React from "react";
 
 const Posts = ({ postsData }) => {
+  const refTextarea = React.createRef();
+  const alertValue = () => {
+    const textareaValue = refTextarea.current.value;
+    alert(textareaValue);
+  };
+
   return (
     <div className={style.posts}>
       <p>My posts</p>
       <div className={style.addPost}>
-        <textarea></textarea>
-        <button>Add post</button>
+        <textarea ref={refTextarea}></textarea>
+        <button onClick={alertValue}>Add post</button>
       </div>
       {postsData.map((post) => (
         <Post text={post.text} likesCount={post.likesCount} />
