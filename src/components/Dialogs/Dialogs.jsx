e6ec194d43b3bb70ec0185903ebba3368c044f85
Alex Volo <alex_volo@bk.ru>
2023-06-19 14:12:15 +0900

    Added connect on MessageInput&Posts

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,27 +3,26 @@ import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
 import MessageInputContainer from "./MessageInput/MessageInputContainer";
 
+const Dialogs = ({ dialogsState }) => {
+    const dialogElements = dialogsState.dialogsData.map((dia) => (
+        <DialogItem name={dia.name} id={dia.id} ava={dia.ava} />
+    ));
+    const messageElements = dialogsState.messagesData.map((mes) => (
+        <Message message={mes.message} />
+    ));
 
-const Dialogs = ({ dialogsState, dispatch }) => {
-  const dialogElements = dialogsState.dialogsData.map((dia) => (
-    <DialogItem name={dia.name} id={dia.id} ava={dia.ava} />
-  ));
-  const messageElements = dialogsState.messagesData.map((mes) => (
-    <Message message={mes.message} />
-  ));
-
-  return (
-    <div className={style.dialogs_wrapper}>
-      <header className={style.header}>
-        <h1>Dialogs</h1>
-      </header>
-      <div className={style.dialogs}>{dialogElements}</div>
-      <div className="messages">
-        {messageElements}
-        <MessageInputContainer dispatch={dispatch} dialogsState={dialogsState} />
-      </div>
-    </div>
-  );
+    return (
+        <div className={style.dialogs_wrapper}>
+            <header className={style.header}>
+                <h1>Dialogs</h1>
+            </header>
+            <div className={style.dialogs}>{dialogElements}</div>
+            <div className="messages">
+                {messageElements}
+                <MessageInputContainer />
+            </div>
+        </div>
+    );
 };
 
 export default Dialogs;
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -2,39 +2,39 @@ const ADD_POST = "ADD-POST";
 const TRANSMIT_TEXT = "TRANSMIT-TEXT";
 
 const initialState = {
-  postsData: [
-    { text: "Пусть здесь будет текст", likesCount: 0 },
-    { text: "Разные посты", likesCount: 20 },
-    { text: "Будут лежать", likesCount: 5 },
-    { text: "Здесь", likesCount: 7 },
-  ],
-  textareaValue: "",
+    postsData: [
+        { text: "Пусть здесь будет текст", likesCount: 0 },
+        { text: "Разные посты", likesCount: 20 },
+        { text: "Будут лежать", likesCount: 5 },
+        { text: "Здесь", likesCount: 7 },
+    ],
+    textareaValue: "",
 };
 
 const profileReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "TRANSMIT-TEXT":
+    switch (action.type) {
+        case "TRANSMIT-TEXT":
+            const newState = { ...state, textareaValue: action.str };
+            return newState;
 
-      state.textareaValue = action.str;
-      break;
-    case "ADD-POST":
-      const post = {
-        text: state.textareaValue,
-        likesCount: 0,
-      };
-      state.postsData.push(post);
-      state.textareaValue = "";
-      break;
-    default:
-      break;
-  }
-  return state;
+        case "ADD-POST":
+            const post = {
+                text: state.textareaValue,
+                likesCount: 0,
+            };
+            state.textareaValue = "";
+            return { ...state, postsData: [...state.postsData, post] };
+
+        default:
+            break;
+    }
+    return state;
 };
 
 export const addPostActionCreator = () => ({ type: ADD_POST });
 export const transmitTextActionCreator = (text) => ({
-  type: TRANSMIT_TEXT,
-  str: text,
+    type: TRANSMIT_TEXT,
+    str: text,
 });
 
 export default profileReducer;
